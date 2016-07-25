c6f596a4536969912346696651c57c3d751c3d64
Sean Capelle <seancapelle@gmail.com>
2016-07-25 14:16:47 -0400

    Added API pull

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,11 +1,44 @@
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "limit=10&api_key=dc6zaTOxFJmzC";
-var movie = "batman";
+
+
+
+$('button').on('click', function() {
+
+var movie = "star wars";
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC&limit=10";
+
 
  $.ajax({
     url: queryURL,
     method: 'GET'
  	})
+
     .done(function(response) {
 
     	console.log(response);
-    }
+
+    	var results = response.data;
+
+    	for (var i = 0; i < results.length; i++) {
+
+    		var movieDiv = $('<div>');
+
+    		var p = $('<p>');
+
+    		var rating = results[i].rating.toUpperCase();
+
+    		p.text("Rated " + rating);
+
+    		var movieImage = $('<img>');
+
+            movieImage.attr("src", results[i].images.fixed_height.url);
+
+            movieDiv.append(p);
+
+            movieDiv.append(movieImage);
+
+            $('#movies').prepend(movieDiv);
+                
+            }
+
+    	})
+    });

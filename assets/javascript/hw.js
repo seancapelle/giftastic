862058958119b48dbf59c22dd060be6edb84a6e9
Sean Capelle <seancapelle@gmail.com>
2016-07-26 21:14:05 -0400

    Fixed button function

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -2,26 +2,32 @@ var titles = ["Star Wars", "Back to the Future", "Ghostbusters", "Indiana Jones"
 
 for (var i = 0; i < titles.length; i++) {
 
-	var button = $('<button data-movie="titles[i]">').append(titles[i]);
+	var button = $('<button data-movie=' +titles[i] + '>').append(titles[i]);
 	$('#movieButtons').append(button);
 }
 
 $('button').on('click', function() {
 
 var movie = $(this).data('movie');
+console.log (this);
+console.log ($(this));
 var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-
+//Ajax call
  $.ajax({
     url: queryURL,
     method: 'GET'
  	})
 
+	//Ajax response
     .done(function(response) {
 
     	console.log(response);
 
     	var results = response.data;
+
+    	//Empty out previous gifs
+    	$('#movies').empty();
 
     	for (var i = 0; i < results.length; i++) {
 
@@ -31,7 +37,14 @@ var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6z
 
     		var rating = results[i].rating.toUpperCase();
 
-    		p.text("Rated " + rating);
+    		//Tests if rating is given
+    		if (rating == ''){
+
+    			p.text("Not rated");
+    		}
+    		else {
+    			p.text("Rated " + rating);
+    		}
 
     		var movieImage = $('<img>');
 
